7698bea3420911e029cf7887160da7254582c018
Benjamin <guiastrennec@gmail.com>
2017-03-25 19:43:41 +0100

    Package cleanup

--- R/tmp/Untitled.R
+++
@@ -1,6 +0,0 @@
-DiagrammeR::render_graph(define_graph(
-  define_comp_layout(qmd_info = examples$threecomp, scaling = TRUE, color_scaling = 'dqmd', comp_scale_fun = function(x) { x/20 })
-  ))
-
-qmd(qmd_info = examples$threecomp, scaling = TRUE, color_scaling = 'dqmd', comp_scale_fun = function(x) { x/20 }, unscaled_color = 'grey30')
-
