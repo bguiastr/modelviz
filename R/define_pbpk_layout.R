f246ca131f4d80a54f5c0d82f77f30b6af2b6f6e
Benjamin <guiastrennec@gmail.com>
2017-03-26 22:18:10 +0200

    Compatibility with DiagrammeR 9.1

--- R/define_pbpk_layout.R
+++ R/define_pbpk_layout.R
@@ -28,57 +28,58 @@ define_pbpk_layout <- function(comp              = NULL,
                                artery_comp_label = 'arterial') {
 
   # Check inputs ------------------------------------------------------------
-  if(is.null(comp) | is.null(arrow)) {
+  if (is.null(comp) | is.null(arrow)) {
     stop('Arguments \"comp\" and \"arrow\" required.')
   }
 
-  vein_comp   <- comp$node[grepl(toupper(vein_comp_label), toupper(comp$label))]
-  artery_comp <- comp$node[grepl(toupper(artery_comp_label), toupper(comp$label))]
+  vein_comp   <- comp$id[grepl(toupper(vein_comp_label), toupper(comp$label))]
+  artery_comp <- comp$id[grepl(toupper(artery_comp_label), toupper(comp$label))]
 
-  if(length(vein_comp) == 0) {
+  if (length(vein_comp) == 0) {
     stop(paste('Vein compartment:', vein_comp_label, 'could not be found.'))
-  } else if(length(artery_comp) == 0) {
+  } else if (length(artery_comp) == 0) {
     stop(paste('Artery compartment:', artery_comp_label, 'could not be found.'))
   }
 
   # Reasign rank
-  comp$rank[comp$node ==  vein_comp]   <- 1
-  comp$rank[comp$node ==  artery_comp] <- 9999
-  comp$rank[!comp$rank %in% c(1,9999)] <- 2
+  comp$rank[comp$id == vein_comp]   <- 1
+  comp$rank[comp$id == artery_comp] <- 9999
+  comp$rank[!comp$rank %in% c(1, 9999)] <- 2
 
   # Set pbpk color mode
-  if(pbpk_color == TRUE) {
-    if('fillcolor' %in% colnames(comp)) {
-      comp[comp$node == artery_comp,'fillcolor'] <- '#FF8080'      # red
-      comp[comp$node == vein_comp,'fillcolor']   <- 'deepskyblue3' # blue
+  if (pbpk_color == TRUE) {
+    if ('fillcolor' %in% colnames(comp)) {
+      comp[comp$id == artery_comp, 'fillcolor'] <- '#FF8080'      # red
+      comp[comp$id == vein_comp, 'fillcolor']   <- 'deepskyblue3' # blue
     } else {
-      comp[comp$node == artery_comp,c('color', 'fontcolor')] <- '#FF8080'      # red
-      comp[comp$node == vein_comp,c('color', 'fontcolor')]   <- 'deepskyblue3' # blue
+      comp[comp$id == artery_comp, c('color', 'fontcolor')] <- '#FF8080'      # red
+      comp[comp$id == vein_comp, c('color', 'fontcolor')]   <- 'deepskyblue3' # blue
     }
 
     arrow[arrow$from == artery_comp | arrow$to == artery_comp,
-          c('color', 'fontcolor')]     <- '#FF8080'      # red
+          c('color', 'fontcolor')] <- '#FF8080'      # red
     arrow[arrow$to == vein_comp | arrow$from == vein_comp,
-          c('color', 'fontcolor')]     <- 'deepskyblue3' # blue
+          c('color', 'fontcolor')] <- 'deepskyblue3' # blue
   }
 
   # Handle intermediary comp
-  out_comp  <- comp$node[comp$style == 'invisible']
+  out_comp  <- comp$id[comp$style == 'invisible']
   move_comp <- arrow[!arrow$from %in% c(vein_comp, artery_comp) &
                        !arrow$to %in% c(vein_comp, artery_comp, out_comp), c('from', 'to')]
-  comp$rank[comp$node %in% move_comp$from] <- comp$rank[comp$node %in% move_comp$to] + 1
+  comp$rank[comp$id %in% move_comp$from] <- comp$rank[comp$id %in% move_comp$to] + 1
   ## Add: if parent move the out_comp will also move
 
   # Add invisible nodes and arrows to force layout
-  comp_edit <- DiagrammeR::combine_nodes(comp,
-                                         data.frame(node  = paste0('I', 1:length(unique(comp$rank))),
-                                                    rank  = sort(unique(comp$rank)),
-                                                    style = 'invisible'))
+  hidden_nodes <- 1:length(unique(comp$rank)) + max(comp$id)
+  comp_edit <- DiagrammeR::combine_ndfs(comp,
+                                        data.frame(id  = hidden_nodes,
+                                                   rank  = sort(unique(comp$rank)),
+                                                   style = 'invisible'))
 
-  arrow_edit <- DiagrammeR::combine_edges(arrow,
-                                          data.frame(from  = paste0('I', 1:(length(unique(comp$rank))-1)),
-                                                     to    = paste0('I', 2:length(unique(comp$rank))),
-                                                     style = 'invis'))
+  arrow_edit <- DiagrammeR::combine_edfs(arrow,
+                                         data.frame(from  = hidden_nodes[1:(length(hidden_nodes) - 1)],
+                                                    to    = hidden_nodes[2:length(hidden_nodes)],
+                                                    style = 'invis'))
 
   pbpk <- list(comp  = comp_edit, arrow = arrow_edit)
 
