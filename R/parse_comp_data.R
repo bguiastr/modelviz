9d2c4a51013ac865e9c03b96775ce80d5b906a57
Benjamin <guiastrennec@gmail.com>
2017-03-30 11:57:10 +0200

    improved compartment label parsing

--- R/parse_comp_data.R
+++ R/parse_comp_data.R
@@ -5,24 +5,24 @@ parse_comp_data <- function(mod_file   = NULL,
                             verbose    = FALSE) {
 
   # Check inputs ------------------------------------------------------------
-  if(is.null(advan) || is.null(trans)) {
+  if (is.null(advan) || is.null(trans)) {
     stop('Arguments \"advan\" and \"trans\" required.')
   }
 
-  if(!advan %in% c(1:4,11:12) && (is.null(mod_file) || is.null(parsed_ext))) {
+  if (!advan %in% c(1:4,11:12) && (is.null(mod_file) || is.null(parsed_ext))) {
     stop('Argument \"mod_file\" and \"parsed_ext\" required when DES is used.')
   }
 
-  if(advan == 10) {
+  if (advan == 10) {
     stop('ADVAN 10 is not currently supported by modelviz.')
   }
 
-  if(trans > 4) {
+  if (trans > 4) {
     stop('TRANS > 4 are not currently supported by modelviz.')
   }
 
   # Get compartment labels --------------------------------------------------
-  if(advan %in% c(1:4, 11:12)) {
+  if (advan %in% c(1:4, 11:12)) {
     comp_labels <- switch(as.character(advan),
                           '1'  = c('Central'),
                           '2'  = c('Depot', 'Central'),
@@ -34,20 +34,36 @@ parse_comp_data <- function(mod_file   = NULL,
     # When DES is used
     comp_labels <- mod_file$CODE[mod_file$ABREV == 'MOD']
 
-    if(any(grepl('NCOMP\\s*=', comp_labels))) {
-      comp_labels  <- as.numeric(gsub('.+=\\s*', '', comp_labels[grepl('NCOMP.*=', comp_labels)]))
+    if (any(grepl('NCOMP[[:alpha:]]*\\s*=\\s*\\d+', comp_labels))) {
+      #comp_labels  <- as.numeric(gsub('.+=\\s*', '', comp_labels[grepl('NCOMP[[:alpha:]]*\\s*=\\s*\\d+', comp_labels)]))
+      comp_labels  <- comp_labels[grepl('NCOMP[[:alpha:]]*\\s*=\\s*\\d+', comp_labels)]
+
+      if (length(comp_labels) != 1) {
+        stop('failed to get the number of compartments from the provided model try using the standard format e.g. $MODEL NCOMP = 8.')
+      }
+
+      comp_labels  <- regmatches(comp_labels, regexpr('NCOMP[[:alpha:]]*\\s*=\\s*\\d+', comp_labels))
+      comp_labels  <- as.numeric(gsub('\\D', '', comp_labels))
+
+      if (is.na(comp_labels)) {
+        stop('failed to get the number of compartments from the provided model try using the standard format e.g. $MODEL NCOMP = 8.')
+      }
+
       comp_labels  <- paste('Comp', 1:comp_labels)
-    } else if(all(grepl('^\\s*COMP\\s*=', comp_labels))) {
-      comp_labels  <- gsub('^.+\\(|,.*$|\\).*$', '', comp_labels)
-      comp_labels  <- paste0(toupper(substr(comp_labels, 1, 1)),
-                             tolower(substr(comp_labels, 2, nchar(comp_labels))))
+
+    } else if (any(grepl('COMP\\s*=', comp_labels))) {
+      comp_labels  <- regmatches(comp_labels, gregexpr('COMP\\s*=\\s*\\((\\s|\\w)*\\)', comp_labels))
+      comp_labels  <- unlist(comp_labels)
+      comp_labels  <- gsub('COMP\\s*=\\s*\\(|\\)', '', comp_labels)
+      comp_labels  <- gsub('\\s+', '\n', comp_labels)
+
     } else {
       comp_labels  <- NULL
     }
   }
 
   # Get compartment scaling parameters --------------------------------------
-  if(advan %in% c(1:4, 11:12)) {
+  if (advan %in% c(1:4, 11:12)) {
     prm_template <- data.frame(advan  = rep(c(1:4, 11:12), each = 2),
                                config = c(1, 2),
                                vc     = c('VC', 'V', 'VC', 'V', 'VC', 'V1', 'VC', 'V2', 'VC', 'V1', 'VC', 'V2'),
@@ -58,7 +74,7 @@ parse_comp_data <- function(mod_file   = NULL,
                                           prm_template$vc == intersect(colnames(parsed_ext$theta), prm_template$vc),
                                         c('vc', 'vp1', 'vp2')])
 
-    if(length(prm_template) == 0) {
+    if (length(prm_template) == 0) {
       msg(paste0('Warning: failed to create \"comp_prm\" for ADVAN ', advan, '.'), verbose)
     }
 
